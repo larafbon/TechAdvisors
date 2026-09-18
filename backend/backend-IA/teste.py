d56018400ebfde5e1cb775638f50506fee0f7566
larafbon <lara.bonette@escola.pr.gov.br>
2026-09-18 11:33:56 -0300

    Add files via upload

---
+++ backend/backend-IA/teste.py
@@ -0,0 +1,15 @@
+#importação
+import csv
+import pathlib from Path
+
+#busca de csv
+
+CAMINHO = "../TechAdivisors/database/dados.csv"
+
+
+def main():
+    
+    
+    
+    if __name__ == __main__:
+        main()
