730d97bb4746c5d2ae0170a9f5c07beda8349b65
marianacuarts <mariana.cuartas365@gmail.com>
2026-09-22 13:34:09 +0000

    Rota GET testada e validada no Postman

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
-from database import engine, Base
-import routes.recursos as recursos
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, Base, get_db
+import models
+import schemas
 
-app = FastAPI(title="TechAdvisors API")
+# Cria as tabelas no Supabase caso ainda não existam
+Base.metadata.create_all(bind=engine)
 
-# Registra as rotas
-app.include_router(recursos.router)
+app = FastAPI(title="API TechAdvisors")
 
-@app.get("/")
-def home():
-    return {"mensagem": "API TechAdvisors rodando com sucesso!"}
+
+# --- Rotas para ConfiguracaoAcessibilidade (Projeto Oficial) ---
+
+@app.get("/configuracoes_acessibilidade", response_model=list[schemas.ConfiguracaoAcessibilidadeResponse])
+def listar_configuracoes(db: Session = Depends(get_db)):
+    return db.query(models.ConfiguracaoAcessibilidade).all()
+
+
+# --- Rotas de Aprendizagem (Categoria) ---
+
+@app.get("/categoria", response_model=list[schemas.CategoriaResponse])
+def listar_categorias(db: Session = Depends(get_db)):
+    return db.query(models.Categoria).all()
