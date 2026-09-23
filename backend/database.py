2751eeb782670c10664dcfca345084596e57cffd
marianacuarts <mariana.cuartas365@gmail.com>
2026-09-23 12:22:14 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # URL Oficial do Transaction Pooler IPv4
-DATABASE_URL = "postgresql://postgres.zarnjgllibjujgkrxnfv:TechAdvisors2026@aws-0-ca-central-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql://postgres.zarnjgllibjujgkrxnfv:password@aws-0-ca-central-1.pooler.supabase.com:6543/postgres"
 
 # Argumentos para evitar travamento de conexão no Codespaces/Supabase
 engine = create_engine(
